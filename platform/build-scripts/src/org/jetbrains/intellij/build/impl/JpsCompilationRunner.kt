fc530b72d55dee87d6ab4380d5d0b23118b96b4d
detachhead <detachhead@users.noreply.github.com>
2026-01-20 21:10:56 +1000

    Revert "IJI-3417 installers.cmd/tests.cmd should use Bazel to run"
    This reverts commit 8a4d4e0a

--- platform/build-scripts/src/org/jetbrains/intellij/build/impl/JpsCompilationRunner.kt
+++ platform/build-scripts/src/org/jetbrains/intellij/build/impl/JpsCompilationRunner.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.Dispatchers
 import org.jetbrains.intellij.build.BuildOptions
 import org.jetbrains.intellij.build.CompilationContext
 import org.jetbrains.intellij.build.impl.logging.jps.withJpsLogging
-import org.jetbrains.intellij.bazelEnvironment.BazelRunfiles
 import org.jetbrains.intellij.build.telemetry.TraceManager
 import org.jetbrains.intellij.build.telemetry.TraceManager.spanBuilder
 import org.jetbrains.intellij.build.telemetry.use
@@ -123,10 +122,6 @@ internal class JpsCompilationRunner(private val context: CompilationContext) {
     resolveProjectDependencies: Boolean = false,
     canceledStatus: CanceledStatus = CanceledStatus.NULL,
   ) = context.withCompilationLock {
-    require(!BazelRunfiles.isRunningFromBazel) {
-      "Running JPS compiler is not supported when running from Bazel."
-    }
-
     val compilationData = context.compilationData
 
     val forceBuild = !context.options.incrementalCompilation || !context.compilationData.isIncrementalCompilationDataAvailable()
